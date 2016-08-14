1c2d7feed32e71eb454c40f2048def5f3140393e
Oliver Lundquist <oliver@kurashicom.jp>
2016-08-14 21:27:34 +0200

    initial commit

---
+++ app/app.routing.ts
@@ -0,0 +1,30 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+  path: 'detail/:id',
+  component: HeroDetailComponent
+},
+
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  }
+
+
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
